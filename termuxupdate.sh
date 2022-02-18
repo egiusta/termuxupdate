d66a2fa4af0a4ee13045bf2f8045b5372fcab94e
Emin ( eGi ) <99421821+egiusta@users.noreply.github.com>
2022-02-19 00:07:15 +0400

    Create termuxupdate.sh

---
+++ termuxupdate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+pkg install git -y
+pkg install python python2 -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install pip pip2 -y
+pkg install vim -y
+pkg install nano -y
+pkg install cat -y
+pkg install figlet -y
+pkg install cmatrix -y
+pkg install perl -y
+apt update
+apt upgrade -y
+clear
+figlet Pofig
